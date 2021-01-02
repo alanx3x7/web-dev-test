4e53d01439dd411e70a74ffa003b6b262302ec44
Alan Lai <alanx3x7@gmail.com>
2021-01-02 15:41:35 -0500

    Updates

---
+++ precache-manifest.d62534b3b6c191a5c337346f28ddd9ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52ccd060bfc7ad0f4f53136a2f17dd1",
+    "url": "/web-dev-test/index.html"
+  },
+  {
+    "revision": "144d5977913765ecf725",
+    "url": "/web-dev-test/static/js/2.772c138d.chunk.js"
+  },
+  {
+    "revision": "6bb833b17d39304afafd",
+    "url": "/web-dev-test/static/js/main.d3542ea7.chunk.js"
+  },
+  {
+    "revision": "a4602e09f48d2bb8bb90",
+    "url": "/web-dev-test/static/js/runtime-main.bbbad2fa.js"
+  }
+]);
